7fe703a992789e80a958748a72e2c282824f3052
vishal366 <vishal.rajkotia@gmail.com>
2019-08-06 20:50:59 +0530

    firstcomit

--- Simple.java
+++ Simple.java
@@ -2,5 +2,6 @@ class Simple{
         public static void main(String args[]){
          System.out.println("Hello Java");
          System.out.println("Hello Java");
+         System.out.println("Hello Java1777");
         }
     }
