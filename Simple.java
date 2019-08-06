90fd4c4b9ccb63dff1a17b748cc716c8ce3f897d
vishal366 <52373370+vishal366@users.noreply.github.com>
2019-08-06 22:04:20 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,6 +2,6 @@ class Simple{
         public static void main(String args[]){
          System.out.println("Hello Java");
          System.out.println("Hello Java");
-         System.out.println("Hello Java1777");
+         System.out.println("Hello Java1kkk777");
         }
     }
