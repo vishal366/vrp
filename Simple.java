d27f06b6688c4b47819765b9b2bdb024204b1597
vishal366 <vishal.rajkotia@gmail.com>
2019-08-07 19:21:53 +0530

    new

--- Simple.java
+++ Simple.java
@@ -1,7 +1,17 @@
+<<<<<<< Updated upstream
 class Simple{
         public static void main(String args[]){
          System.out.println("Hello Java");
          System.out.println("Hello Java");
-         System.out.println("Hello Java1kkk777");
+         System.out.println("Hello Jannnnva1kkk777");
         }
     }
+=======
+class Simple{
+        public static void main(String args[]){
+         System.out.println("Hello Java");
+         System.out.println("Hello Java");
+         System.out.println("Hello Java177iiii7");
+        }
+    }
+>>>>>>> Stashed changes
