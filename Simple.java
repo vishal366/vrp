7154d8ee4c0d3b48762cef807b9afb1b55e04681
vishal366 <vishal.rajkotia@gmail.com>
2019-08-09 21:44:33 +0530

    new

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 
 class Simple{
         public static void main(String args[]){
-         System.out.println("Hello Jaiiva");
+         System.out.println("Hello Jlllllaiiva");
          System.out.println("Hello Java");
 
          System.out.println("Hello Java");
