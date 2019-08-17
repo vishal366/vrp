e9c0dae3cf0c0dcbb28fa0d29134f388076c3822
vishal366 <vishal.rajkotia@gmail.com>
2019-08-17 16:33:47 +0530

    kkkk

--- Simple.java
+++ Simple.java
@@ -2,7 +2,7 @@
 class Simple{
         public static void main(String args[]){
          System.out.println("Hello Jaiiva");
-         System.out.println("Hello Java");
+         System.out.println("Hello Javkkkkka");
 
          System.out.println("Hello Java");
 
